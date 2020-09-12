a956ad03e8a97a863b98a0a915f49b4355330516
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2020-09-12 18:29:40 -0500

    for loop

--- Chap02/for.cpp
+++ Chap02/for.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
+
+    // char string[] = "abcdefg";
+    // for( int i = 0; string[i]; ++i ) {
+    //     printf("i is %c\n", string[i]);
+    // } 
+
+    char string[] = "abcdefg";
+    for( char * cp = string; *cp; ++cp ) {
+        printf("*cp is %c\n", *cp);
+    } 
     // basic for loop
     for( int i = 0; i < 5; ++i ) {
         printf("i is %d\n", i);
