0e1c7d068777d06b189c335b673ea1d7e9fbd73e
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2020-09-12 18:47:08 -0500

    range based for loop

--- Chap02/for.cpp
+++ Chap02/for.cpp
@@ -18,5 +18,9 @@ int main()
     for( int i = 0; i < 5; ++i ) {
         printf("i is %d\n", i);
     }
+    int array[] = {1, 2, 3, 4, 5};
+    for (int i: array){
+       printf("i is %d\n", i); 
+    }
     return 0;
 }
